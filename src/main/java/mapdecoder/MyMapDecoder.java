90ee6c00117136807bff8fe269381401037b7897
rudojsru <38580647+rudojsru@users.noreply.github.com>
2019-03-03 18:15:54 +0100

    Update MyMapDecoder.java

--- src/main/java/mapdecoder/MyMapDecoder.java
+++ src/main/java/mapdecoder/MyMapDecoder.java
@@ -7,10 +7,22 @@ public class MyMapDecoder implements MapDecoder {
 
 	@Override
 	public Map<String, String> decode(String s) {
-		throw new UnsupportedOperationException("Please implement me!");
+		  //throw new UnsupportedOperationException("Please implement me!");
+        if (s == null) {
+            return null;
+        }
+        Map<String, String> expected = new HashMap<>();
+        String[] result = s.split("&");
+        if (result.length <= 0) {
+            return expected;
+        }
 
+        for (int i = 0; i < result.length; i++) {
+            String[] kuczRes = result[i].split("=");
+            expected.put(kuczRes[0], kuczRes[1]);
 
-	}
-
+        }
+        return expected;
+    }
 
 }
