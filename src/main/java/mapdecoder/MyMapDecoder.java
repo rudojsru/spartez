fc035f5c7b9ec2610059d569bac10c9202b505b4
rudojsru <38580647+rudojsru@users.noreply.github.com>
2019-03-03 17:50:26 +0100

    Update MyMapDecoder.java

--- src/main/java/mapdecoder/MyMapDecoder.java
+++ src/main/java/mapdecoder/MyMapDecoder.java
@@ -1,8 +1,4 @@
-/*
-   This file is provided exclusively for the purpose of Spartez Online Assessment.
-   Unauthorized distribution of this file, derived work or information about its
-   content, via any medium, is strictly prohibited.
- */
+
 package main.java.mapdecoder;
 
 import java.util.Map;
